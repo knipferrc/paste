b0e0f6c2c83115b3399c58a59eb16c791c8eaae6
knipferrc <knipferrc@gmail.com>
2017-09-07 12:38:20 -0400

    setup graphql server

---
+++ server/src/api/resolvers.js
@@ -0,0 +1,10 @@
+const resolvers = {
+  Query: {
+    version: () => 1,
+  },
+  Mutation: {
+    version: () => 1,
+  },
+}
+
+export default resolvers
--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,24 @@
+import { graphiqlKoa, graphqlKoa } from 'apollo-server-koa'
+
 import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import compression from 'koa-compress'
+import koaRouter from 'koa-router'
+import schema from './api/schema'
 
 const app = new Koa()
+const router = new koaRouter()
+const PORT = 5000
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa'
-})
+app.use(bodyParser())
+app.use(compression())
 
-app.listen(3000)
+router.post('/api', graphqlKoa({ schema }))
+router.get('/api', graphqlKoa({ schema }))
+
+router.get('/graphiql', graphiqlKoa({ endpointURL: '/api' }))
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(PORT)
