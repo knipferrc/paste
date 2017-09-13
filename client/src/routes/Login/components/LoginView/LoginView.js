4dddfd4343b085c267b14fd9ca7ddfb36af9d056
knipferrc <knipferrc@gmail.com>
2017-09-13 10:10:51 -0400

    pushstate server

--- client/src/routes/Login/components/LoginView/LoginView.js
+++ client/src/routes/Login/components/LoginView/LoginView.js
@@ -5,20 +5,17 @@ import LoginForm from '../../forms/LoginForm'
 import React from 'react'
 
 const LoginView = () => (
-    <Grid
-      textAlign="center"
-      padded
-    >
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as="h1" textAlign="center">
-          Log-in to your account
-        </Header>
-        <LoginForm />
-        <Message>
-          New to #pastey!? <Link to="/signup">Sign Up</Link>
-        </Message>
-      </Grid.Column>
-    </Grid>
+  <Grid textAlign="center" padded>
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as="h1" textAlign="center">
+        Log-in to your account
+      </Header>
+      <LoginForm />
+      <Message>
+        New to #pastey!? <Link to="/signup">Sign Up</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
 )
 
 export default LoginView
--- client/src/routes/SignUp/components/SignUpView/SignUpView.js
+++ client/src/routes/SignUp/components/SignUpView/SignUpView.js
@@ -5,20 +5,17 @@ import React from 'react'
 import SignUpForm from '../../forms/SignUpForm'
 
 const SignUpView = () => (
-    <Grid
-      textAlign="center"
-      padded
-    >
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as="h1" textAlign="center">
-          Register for an account today
-        </Header>
-        <SignUpForm />
-        <Message>
-          Already A Member? <Link to="/login">Login</Link>
-        </Message>
-      </Grid.Column>
-    </Grid>
+  <Grid textAlign="center" padded>
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as="h1" textAlign="center">
+        Register for an account today
+      </Header>
+      <SignUpForm />
+      <Message>
+        Already A Member? <Link to="/login">Login</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
 )
 
 export default SignUpView
