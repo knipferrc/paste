81279813cb63a33aef983fa17124f49dcb855db9
knipferrc <knipferrc@gmail.com>
2017-09-18 11:29:10 -0400

    package cleanup

--- frontend/src/routes/EditBlog/components/TextEditor/TextEditor.js
+++ frontend/src/routes/EditBlog/components/TextEditor/TextEditor.js
@@ -1,8 +1,6 @@
 import React, { PureComponent } from 'react'
 
-import { Editor } from 'slate-react'
 import EditorControls from './EditorControls'
-import { State } from 'slate'
 import styled from 'styled-components'
 
 const EditorContainer = styled.div`
@@ -11,46 +9,16 @@ const EditorContainer = styled.div`
   padding: 10px;
 `
 
-const initialState = State.fromJSON({
-  document: {
-    nodes: [
-      {
-        kind: 'block',
-        type: 'paragraph',
-        nodes: [
-          {
-            kind: 'text',
-            ranges: [
-              {
-                text: 'Create your blog here!!!'
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-})
-
-class TextEditor extends PureComponent {
-  state = {
-    state: initialState
-  }
-
-  onChange = ({ state }) => {
-    this.setState({ state })
-  }
-
+export default class TextEditor extends PureComponent {
   render() {
     return (
       <div>
         <EditorControls />
         <EditorContainer>
-          <Editor state={this.state.state} onChange={this.onChange} />
+          Edit here
         </EditorContainer>
       </div>
     )
   }
 }
 
-export default TextEditor
