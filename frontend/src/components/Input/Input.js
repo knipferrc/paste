f9630e4b19e8c082fc5724c249e40e1d83de7d32
knipferrc <knipferrc@gmail.com>
2017-09-18 09:24:45 -0400

    use pure component

--- frontend/src/components/Input/Input.js
+++ frontend/src/components/Input/Input.js
@@ -1,45 +1,44 @@
+import React, { PureComponent } from 'react'
+
 import { Form } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
-import React from 'react'
 
-const propTypes = {
-  fluid: PropTypes.bool,
-  icon: PropTypes.string,
-  iconPosition: PropTypes.string,
-  placeholder: PropTypes.string,
-  required: PropTypes.bool,
-  label: PropTypes.bool,
-  meta: PropTypes.object,
-  input: PropTypes.object,
-  type: PropTypes.string
+export default class Input extends PureComponent {
+  static propTypes = {
+    fluid: PropTypes.bool,
+    icon: PropTypes.string,
+    iconPosition: PropTypes.string,
+    placeholder: PropTypes.string,
+    required: PropTypes.bool,
+    label: PropTypes.bool,
+    meta: PropTypes.object,
+    input: PropTypes.object,
+    type: PropTypes.string
+  }
+  render() {
+    const {
+      input,
+      meta: { error, touched },
+      fluid,
+      icon,
+      iconPosition,
+      placeholder,
+      required,
+      label,
+      type
+    } = this.props
+    return (
+      <Form.Input
+        fluid={fluid}
+        type={type}
+        error={touched && error}
+        icon={icon}
+        label={label}
+        required={required}
+        iconPosition={iconPosition}
+        placeholder={placeholder}
+        {...input}
+      />
+    )
+  }
 }
-
-const Input = ({
-  input,
-  meta: { error, touched },
-  fluid,
-  icon,
-  iconPosition,
-  placeholder,
-  required,
-  label,
-  type
-}) => {
-  return (
-    <Form.Input
-      fluid={fluid}
-      type={type}
-      error={touched && error}
-      icon={icon}
-      label={label}
-      required={required}
-      iconPosition={iconPosition}
-      placeholder={placeholder}
-      {...input}
-    />
-  )
-}
-
-Input.propTypes = propTypes
-
-export default Input
