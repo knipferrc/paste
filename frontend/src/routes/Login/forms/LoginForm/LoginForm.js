bebf0ebf5cf29169440fdd45f024c7060b1fadee
knipferrc <knipferrc@gmail.com>
2017-09-18 12:33:03 -0400

    use class

--- frontend/src/routes/Login/forms/LoginForm/LoginForm.js
+++ frontend/src/routes/Login/forms/LoginForm/LoginForm.js
@@ -1,59 +1,60 @@
 import { Button, Form, Header, Message, Segment } from 'semantic-ui-react'
 import { Field, reduxForm } from 'redux-form'
+import React, { PureComponent } from 'react'
 
 import Input from 'components/Input'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import React from 'react'
 import { required } from 'lib/validations'
 
-const propTypes = {
-  handleSubmit: PropTypes.func
-}
+class LoginForm extends PureComponent {
+  static propTypes = {
+    handleSubmit: PropTypes.func
+  }
 
-const login = values => {
-  alert(JSON.stringify(values))
-}
+  submit = values => {
+    console.log(values)
+  }
 
-const LoginForm = ({ handleSubmit }) => {
-  return (
-    <Form size="large" onSubmit={handleSubmit(login)}>
-      <Segment stacked>
-        <Header as="h1" textAlign="center">
-          Log In to your account
-        </Header>
-        <Field
-          type="email"
-          name="email"
-          icon="mail"
-          validate={required}
-          iconPosition="left"
-          placeholder="E-mail address"
-          component={Input}
-          fluid
-        />
-        <Field
-          type="password"
-          name="password"
-          icon="lock"
-          iconPosition="left"
-          validate={required}
-          placeholder="Password"
-          component={Input}
-          fluid
-        />
-        <Button type="submit" color="teal" fluid size="large">
-          Login
-        </Button>
-        <Message>
-          New to #pastey!? <Link to="/signup">Sign Up</Link>
-        </Message>
-      </Segment>
-    </Form>
-  )
+  render() {
+    const { handleSubmit } = this.props
+    return (
+      <Form size="large" onSubmit={handleSubmit(this.submit)}>
+        <Segment stacked>
+          <Header as="h1" textAlign="center">
+            Log In to your account
+          </Header>
+          <Field
+            type="email"
+            name="email"
+            icon="mail"
+            validate={required}
+            iconPosition="left"
+            placeholder="E-mail address"
+            component={Input}
+            fluid
+          />
+          <Field
+            type="password"
+            name="password"
+            icon="lock"
+            iconPosition="left"
+            validate={required}
+            placeholder="Password"
+            component={Input}
+            fluid
+          />
+          <Button type="submit" color="teal" fluid size="large">
+            Login
+          </Button>
+          <Message>
+            New to #pastey!? <Link to="/signup">Sign Up</Link>
+          </Message>
+        </Segment>
+      </Form>
+    )
+  }
 }
-
-LoginForm.propTypes = propTypes
 
 export default reduxForm({
   form: 'login'
