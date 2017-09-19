beaf0efae08b094510b77684cdcfe5ae773ce104
knipferrc <knipferrc@gmail.com>
2017-09-19 12:10:35 -0400

    authorization header

--- frontend/src/store/client.js
+++ frontend/src/store/client.js
@@ -4,13 +4,20 @@ const networkInterface = createNetworkInterface({
   uri:
     process.env.NODE_ENV === 'production'
       ? 'https://pastey-server.now.sh/api'
-      : 'http://localhost:5000/api',
-  opts: {
-    headers: {
-      credentials: 'include'
-    }
-  }
+      : 'http://localhost:5000/api'
 })
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {};  // Create the header object if needed.
+    }
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('accessToken');
+    req.options.headers.authorization = token ? token : null;
+    next();
+  }
+}]);
 
 const client = new ApolloClient({
   networkInterface: networkInterface
