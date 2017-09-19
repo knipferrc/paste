89441bbd0b93824a04083792d167e3eef16d44ea
knipferrc <knipferrc@gmail.com>
2017-09-19 09:59:42 -0400

    fix logic

--- backend/src/api/mutations/register.js
+++ backend/src/api/mutations/register.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import uuidv4 from 'uuid/v4'
 
 export default async (
   root,
@@ -8,23 +9,21 @@ export default async (
 ) => {
   const duplicateUser = await db
     .collection('users')
-    .find({ email: email })
-    .count()
-  if (duplicateUser !== 1) {
+    .findOne({ email: email })
+  if(!duplicateUser) {
     const saltRounds = 10
-    const hash = bcrypt.hashSync(password, saltRounds)
-    const user = {
+    let secretKey = uuidv4()
+    const hash = await bcrypt.hash(password, saltRounds)
+    const userData = {
       firstName,
       lastName,
       email,
       password: hash
     }
-    await db.collection('users').insertOne(user)
-    const token = jwt.sign(user, process.env.JWT_SECRET, {
-      expiresIn: 60 * 60 * 24
-    })
+    const newUser = await db.collection('users').insertOne(userData)
+    const token = await jwt.sign({ userId: newUser.insertedId, iss: 'https://pastey.now.sh' }, secretKey)
     return token
   } else {
-    throw new Error('Sorry, a user has already registered with that email')
+    throw new Error("Sorry, that email is already in use")
   }
 }
