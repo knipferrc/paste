beb8a9615bb816cc011b9f2cae81e43205fe65fc
knipferrc <knipferrc@gmail.com>
2017-09-25 15:43:35 -0400

    cleanup preview

--- frontend/src/routes/PublishPaste/components/PublishingPreview/PublishingPreview.js
+++ frontend/src/routes/PublishPaste/components/PublishingPreview/PublishingPreview.js
@@ -1,17 +1,29 @@
-import { Loader } from 'semantic-ui-react'
+import { Loader, Segment } from 'semantic-ui-react'
+
 import PropTypes from 'prop-types'
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import styled from 'styled-components'
 
 const propTypes = {
   loading: PropTypes.bool,
   pasteContent: PropTypes.string
 }
 
+const Container = styled.div`margin: 5px;`
+
 const PublishingPreview = ({ loading, pasteContent }) => {
   return (
     <div>
-      {loading ? <Loader active /> : <ReactMarkdown source={pasteContent} />}
+      {loading ? (
+        <Loader active />
+      ) : (
+        <Container>
+          <Segment piled padded>
+            <ReactMarkdown source={pasteContent} />
+          </Segment>
+        </Container>
+      )}
     </div>
   )
 }
