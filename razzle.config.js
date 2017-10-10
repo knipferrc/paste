a2cb7165c225d68b370c1f0dc6a3520e48e4c0a8
knipferrc <knipferrc@gmail.com>
2017-10-10 12:54:29 -0400

    add gql loader

--- razzle.config.js
+++ razzle.config.js
@@ -1,18 +1,13 @@
 module.exports = {
   modify(config, { target, dev }, webpack) {
-    const appConfig = config; // stay immutable here
+    const appConfig = config
 
     appConfig.module.rules.push({
       test: /\.(graphql|gql)$/,
       exclude: /node_modules/,
       loader: 'graphql-tag/loader'
-    });
+    })
 
-    // Change the name of the server output file in production
-    if (target === 'node' && !dev) {
-      appConfig.output.filename = 'custom.js';
-    }
-
-    return appConfig;
-  },
-};
+    return appConfig
+  }
+}
