a0b129b8309617e834751027d63feb9a1ab7d40f
knipferrc <knipferrc@gmail.com>
2017-10-16 17:52:41 -0400

    SAVEPOINT

---
+++ pages/register/index.js
@@ -0,0 +1,11 @@
+import DefaultLayout from 'layouts/DefaultLayout'
+import React from 'react'
+import withData from 'lib/withData'
+
+const Register = () => (
+  <DefaultLayout title="Register">
+    <h1>Register</h1>
+  </DefaultLayout>
+)
+
+export default Register
