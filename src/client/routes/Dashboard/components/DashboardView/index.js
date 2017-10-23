5f095cf1da4f440cf0e3df0f507f0f58b457f49a
knipferrc <knipferrc@gmail.com>
2017-10-23 15:58:42 -0400

    load myPastes

--- src/client/routes/Dashboard/components/DashboardView/index.js
+++ src/client/routes/Dashboard/components/DashboardView/index.js
@@ -1,13 +1,27 @@
 import DefaultLayout from 'client/layouts/DefaultLayout'
+import PropTypes from 'prop-types'
 import React from 'react'
 import hoc from './hoc'
 
-const DashboardView = () => {
+const DashboardView = ({ loading, myPastes }) => {
+  if (loading) {
+    return (
+      <DefaultLayout>
+        <i className="fa fa-spinner fa-spin fa-5x" />
+      </DefaultLayout>
+    )
+  }
+
   return (
     <DefaultLayout>
-      <div>Dashboard</div>
+      <pre>{JSON.stringify(myPastes, null, 2)}</pre>
     </DefaultLayout>
   )
+}
+
+DashboardView.propTypes = {
+  loading: PropTypes.bool,
+  myPastes: PropTypes.array
 }
 
 export default hoc(DashboardView)
